c3a8808e5ebd0f03d3c84bf61071a216039bcc08
BrianSFarfan <brian.sfarfan@gmail.com>
2022-12-04 02:21:50 -0300

    a

--- PAGINA REAL XD/js/nav.js
+++ PAGINA REAL XD/js/nav.js
@@ -1,7 +1,5 @@
 window.onscroll = function() {
     var y = window.scrollY;
-    document.getElementById('y').innerText = y;
-
     if (y > 50) {
         if (window.matchMedia("(min-width: 380px)").matches) {
             document.getElementById("botoNav").className = "checkbtn ";
