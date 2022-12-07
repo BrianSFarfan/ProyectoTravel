68aa8449447831a8e9febd27e05a0725b15c637a
XeonnNK <94070618+XeonnNK@users.noreply.github.com>
2022-12-06 21:35:23 -0300

    Boton de subir pa arriba facha
    Actualizacion del js con el respectivo para que se oculte cuando esta en x punto de la pagina y se muestre asi mismo
    Cambio en los estilos en lo referente al mismo boton
    E implementacion del boton en el index

--- PAGINA REAL XD/js/nav.js
+++ PAGINA REAL XD/js/nav.js
@@ -11,3 +11,17 @@ window.onscroll = function() {
     }
 };
 
+function irArriba(){
+    window.addEventListener('scroll',()=>{
+        var scroll= document.documentElement.scrollTop;
+        console.log(scroll);
+        var botonArriba= document.getElementById('botonArriba')
+        
+        if(scroll>300){
+            botonArriba.style.right= 20 + 'px';
+        } else{
+            botonArriba.style.right = -100+ 'px';
+        }
+    })
+}
+irArriba();
