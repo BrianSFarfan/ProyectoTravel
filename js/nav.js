d85e600353604e975d27071b0baa1b337a623d50
BrianSFarfan <brian.sfarfan@gmail.com>
2022-12-07 20:21:34 -0300

    BOTON ARRIBA CORREJIDO

--- js/nav.js
+++ js/nav.js
@@ -10,25 +10,11 @@ window.onscroll = function() {
         document.getElementById("botoNav").className = "checkbtn";
     }
 
-
-    function irArriba() {
-        window.addEventListener('scroll', () => {
-            var scroll = document.documentElement.scrollTop;
-
-            var botonArriba = document.getElementById('botonArriba')
-
-            if (scroll > 300) {
-                botonArriba.style.right = 20 + 'px';
-            } else {
-                botonArriba.style.right = -100 + 'px';
-            }
-        })
-    }
-    irArriba();
-
     if (y > 200) {
         document.getElementById("NavCompleto").className = "navFixedDark";
+        document.getElementById("botonArriba").className = "btnHaciaArriba";
     } else {
         document.getElementById("NavCompleto").className = "";
+        document.getElementById("botonArriba").className = "btnHaciaArriba btnA-Desactivado";
     }
 };
