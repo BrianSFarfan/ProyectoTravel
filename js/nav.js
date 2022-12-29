b1efc6c1288fb6f2dae3750035f07668997d15df
BrianSFarfan <brian.sfarfan@gmail.com>
2022-12-29 17:56:56 -0300

    Correccion JS

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,8 @@
-window.onscroll = function() {
-    var y = window.scrollY;
-    if (y > 50) {
-        if (window.matchMedia("(min-width: 380px)").matches) {
-            document.getElementById("botoNav").className = "checkbtn ";
-        } else {
-            document.getElementById("botoNav").className = "checkbtn botonArriba";
-        }
+var nav = document.getElementById('NavCompleto');
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('navFixedDark');
     } else {
-        document.getElementById("botoNav").className = "checkbtn";
+        nav.classList.remove('navFixedDark');
     }
-
-    if (y > 200) {
-        document.getElementById("NavCompleto").className = "navFixedDark ";
-        document.getElementById("botonArriba").className = "btnHaciaArriba btnA-activado";
-    } else {
-        document.getElementById("NavCompleto").className = "";
-        document.getElementById("botonArriba").className = "btnHaciaArriba ";
-    }
-};
+});
